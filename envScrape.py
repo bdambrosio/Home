283f503f66027f5a6af910d39643186c7f87d967
bdambrosio <bruce.dambrosio@gmail.com>
2021-08-13 03:48:44 +0100

    updated envScrape to accumulate and only update influx once/10min, like pvScrape

--- envScrape.py
+++ envScrape.py
@@ -10,6 +10,11 @@ from datetime import datetime
 import pytz
 import math
 
+
+intvl_total = {}
+intvl_count = {}
+last_db_update_time = {}
+
 ptz = pytz.timezone('America/Los_Angeles')
 utc = pytz.timezone('UTC')
 now = utc.localize(datetime.utcnow())
@@ -42,7 +47,8 @@ def update_db(topic, value):
 
 #setup to publish to mosquitto broker
 def new_measurement(client, userdata, msg):
-    global last_humidity
+    global intvl_total, intvl_count, last_db_update_time, db
+
     try:
         print (msg.topic, msg.payload)
         topic = msg.topic
@@ -50,33 +56,60 @@ def new_measurement(client, userdata, msg):
         now = utc.localize(datetime.utcnow())
         Time = str(now.astimezone(ptz))[:-13]
         tags = topic.split('/')
-        print (tags)
+
         if 'home' in topic: #need to convert rfm brokered msgs to 'tele' format below!
             data = json.loads(msg.payload)
             measure = data['measure']
-            json_measurement['measurement'] = measure
-            json_measurement['fields']['value'] = data['value']
-            json_measurement['fields']['units'] = 'C'
-            json_measurement['tags']['sys'] = tags[0]
-            json_measurement['tags']['subsys'] = tags[1]
-            if measure == 'hum':
-                last_humidity = data['value']
-            print (json_measurement)
-            db.write_points(json_measurements)
+            if not topic in intvl_total.keys():
+                intvl_total[topic] = 0.0 # start new accumulator
+                intvl_count[topic] = 0
+                last_db_update_time[topic] = time.time() - 601 # because this measurement not in last update
+                print (topic, intvl_total.keys())
+            intvl_total[topic] += data['value']
+            intvl_count[topic] += 1
+            int_time = int(time.time())
+            if int_time - last_db_update_time[topic] > 600: # more than 1 hr
+                try:
+                    json_measurement['measurement'] = measure
+                    intvl_value = intvl_total[topic]/intvl_count[topic]
+                    json_measurement['fields']['value'] = intvl_value
+                    json_measurement['fields']['units'] = 'C'
+                    json_measurement['tags']['sys'] = tags[0]
+                    json_measurement['tags']['subsys'] = tags[1]
+                    db.write_points(json_measurements)
+                except BaseError as e:
+                    print("errror in db write", e)
+
         elif 'tele' in topic and tags[1] == 'sds011' and tags[2] == 'SENSOR': # std tasmota mqtt format
             data = json.loads(msg.payload)
-            json_measurement['measurement'] = 'particulates'
-            json_measurement['fields']['units'] = 'PPM'
-            json_measurement['tags']['sys'] = 'env'
-            json_measurement['tags']['subsys'] = 'SDS011'
-            json_measurement['fields']['value'] = data['SDS0X1']['PM2.5']
-            json_measurement['tags']['subsys2'] = 'PM2.5'
-            print (json_measurement)
-            db.write_points(json_measurements)
-            json_measurement['fields']['value'] = data['SDS0X1']['PM10']
-            json_measurement['tags']['subsys2'] = 'PM10'
-            print (json_measurement)
-            db.write_points(json_measurements)
+            topic25 = topic+'2.5 '
+            topic10 = topic+'10'
+            if not topic in intvl_total.keys():
+                # tasmota reports both values of sds011 in a single msg
+                intvl_total[topic25] = 0.0 # start new accumulator
+                intvl_total[topic10] = 0.0 # start new accumulator
+                intvl_count[topic] = 0
+                last_db_update_time[topic] = time.time() - 601 # because this measurement not in last update
+            intvl_total[topic25] += data['SDS0X1']['PM2.5']
+            intvl_total[topic10] += data['SDS0X1']['PM10']
+            intvl_count[topic] += 1
+            int_time = int(time.time())
+
+            if int_time - last_db_update_time[topic] > 600: # more than 1 hr
+                json_measurement['measurement'] = 'particulates'
+                json_measurement['fields']['units'] = 'PPM'
+                json_measurement['tags']['sys'] = 'env'
+                json_measurement['tags']['subsys'] = 'SDS011'
+                json_measurement['fields']['value'] = intvl_total[topic25]/intvl_count[topic]
+                json_measurement['tags']['subsys2'] = 'PM2.5'
+                db.write_points(json_measurements)
+                json_measurement['fields']['value'] = intvl_total[topic10]/intvl_count[topic]
+                json_measurement['tags']['subsys2'] = 'PM10'
+                db.write_points(json_measurements)
+                intvl_total[topic10] = 0.0
+                intvl_count[topic10] = 0
+                intvl_total[topic10] = 0.0
+                intvl_count[topic10] = 0
         
     except OSError as e:
         print ("exception caught", e)
